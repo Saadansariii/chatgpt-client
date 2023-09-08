f0b24f7b4d912bff596f158cb88429c6d5b8f962
Saad <saadansari74002@gmail.com>
2023-09-08 21:04:35 +0530

    fskdj

--- src/api.js
+++ src/api.js
@@ -1,51 +1,19 @@
-// export const fetchResponse =  async(chat) => {
-//     try {
-//         // after depoloyment you should change the fetch URL below
-//         const response = await fetch('https://gpt-backendd.vercel.app/', { 
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({
-//                 message: chat.map((message)=> message.message).join(" \n ")
-//             })
-//         })
-
-//         const data = await response.json()
-//         return data
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-async function fetchResponse(chat) {
-    const apiUrl = 'https://gpt-backendd.vercel.app/';
-    
-    const headers = {
-        'Content-Type': 'application/json',
-    };
-    
+export const fetchResponse =  async(chat) => {
     try {
-        const response = await fetch(apiUrl, {
+        // after depoloyment you should change the fetch URL below
+        const response = await fetch('https://gpt-backendd.vercel.app/', { 
             method: 'POST',
-            headers: headers,
+            headers: {
+                "Content-Type": "application/json",
+            },
             body: JSON.stringify({
-                message: chat.map((message) => message.message).join(' \n '),
-            }),
-        });
-        
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+                message: chat.map((message)=> message.message).join(" \n ")
+            })
+        })
 
-        const data = await response.json();
-        return data;
+        const data = await response.json()
+        return data
     } catch (error) {
-        console.error('Error:', error);
-        // Handle the error gracefully, e.g., display an error message to the user.
-        throw error;
+        console.log(error);
     }
 }
-
-
-export { fetchResponse }
